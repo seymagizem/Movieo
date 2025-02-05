1f7e9b1126adafd62af44e81277e2fa8ff9bba2b
GizemSivri <sivriseymagizem@gmail.com>
2025-02-05 16:34:06 +0300

    Initial Commit

---
+++ movieo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movieo
+//
+//  Created by şeyma gizem sivri on 5.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movieo/movieoApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieoApp.swift
+//  movieo
+//
+//  Created by şeyma gizem sivri on 5.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct movieoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
